d28d266818d628d3a6f36713ff06f66d18929417
Mar5bar <7798410+Mar5bar@users.noreply.github.com>
2023-11-15 17:43:30 +0000

    Buttons added. Abstraction

--- vpde-inputs.js
+++ vpde-inputs.js
@@ -10,19 +10,13 @@ class VPDEInput extends HTMLElement {
     this.attachedFrames = this.getAttribute("iframe")
       .split(" ")
       .map((frame) => document.getElementById(frame));
-    this.message = { name: this.getAttribute("name") };
+    this.message = {};
     // Specify a custom host if one is provided, otherwise use the default.
     this.host = this.getAttribute("host") || "https://visualpde.com";
-
-    // Add an event listener to the iframe so that it gets sent the current value when loaded.
-    this.attachedFrames.forEach((frame) => {
-      frame.addEventListener("load", this.sendUpdate.bind(this));
-    });
   }
 
   // Send an update to the associated simulation.
   sendUpdate() {
-    this.message.value = this.slider.value;
     this.attachedFrames.forEach((frame) => {
       frame.contentWindow.postMessage(this.message, this.host);
     });
@@ -34,7 +28,8 @@ class VPDESlider extends VPDEInput {
   constructor() {
     super();
 
-    // Modify the message to include the updateParam type.
+    // Modify the message to include the parameter name and the type of message.
+    this.message.name = this.getAttribute("name");
     this.message.type = "updateParam";
 
     // Create a slider and a name tag in a span.
@@ -83,13 +78,97 @@ class VPDESlider extends VPDEInput {
     if (MathJax.typesetPromise != undefined) {
       MathJax.typesetPromise();
     }
+
+    // Add an event listener to the iframe so that it gets sent the current value when loaded.
+    this.attachedFrames.forEach((frame) => {
+      frame.addEventListener("load", this.sendUpdate.bind(this));
+    });
   }
 
   onInput() {
     // Update the slider's value and send an update to the simulation.
     this.slider.style.setProperty("--value", this.slider.value);
+    this.message.value = this.slider.value;
     this.sendUpdate();
   }
 }
 
+class VPDEButton extends VPDEInput {
+  constructor() {
+    super();
+
+    // The button will be in a span.
+    const span = document.createElement("span");
+    span.style.setProperty("white-space", "nowrap");
+    span.classList.add("vpde-button");
+    this.span = span;
+    this.append(span);
+
+    // Listen for click events.
+    this.addEventListener("click", this.onclick.bind(this));
+  }
+
+  onclick() {
+    this.sendUpdate();
+  }
+}
+
+class VPDEReset extends VPDEButton {
+  constructor() {
+    super();
+
+    this.message.type = "resetSim";
+    this.symbol = this.span.appendChild(document.createElement("i"));
+    this.symbol.classList.add("fa-solid", "fa-arrow-rotate-left");
+  }
+}
+
+class VPDEPlayPause extends VPDEButton {
+  constructor() {
+    super();
+
+    // Get the initial state of the button.
+    this.state = this.getAttribute("action") || "play";
+    this.message.type = this.state == "play" ? "playSim" : "pauseSim";
+
+    // Create two symbols, one for play and one for pause, each in wrappers.
+    this.playWrapper = this.span.appendChild(document.createElement("div"));
+    const play = this.playWrapper.appendChild(document.createElement("i"));
+    play.classList.add("fa-solid", "fa-play");
+    this.span.append(this.playWrapper);
+    this.pauseWrapper = this.span.appendChild(document.createElement("div"));
+    const pause = this.pauseWrapper.appendChild(document.createElement("i"));
+    pause.classList.add("fa-solid", "fa-pause");
+    this.span.append(this.pauseWrapper);
+
+    this.setDisplay();
+  }
+
+  onclick() {
+    super.onclick();
+    // Update the button for next click.
+    if (this.state == "play") {
+      this.state = "pause";
+      this.message.type = "pauseSim";
+    } else {
+      this.state = "play";
+      this.message.type = "playSim";
+    }
+    // Configure the displayed symbol.
+    this.setDisplay();
+  }
+
+  setDisplay() {
+    if (this.state == "play") {
+      this.pauseWrapper.style.display = "none";
+      this.playWrapper.style.display = "inline-flex";
+    } else {
+      this.playWrapper.style.display = "none";
+      this.pauseWrapper.style.display = "inline-flex";
+    }
+  }
+}
+
 customElements.define("vpde-slider", VPDESlider);
+customElements.define("vpde-reset", VPDEReset);
+customElements.define("vpde-playpause", VPDEPlayPause);
